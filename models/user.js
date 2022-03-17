479d41f77ba75c2b95b9dea2e861a9683cdf1f8c
Tanmay Modi <54397441+tanmayedevaraj@users.noreply.github.com>
2022-03-17 17:45:24 +0530

    final commit
    all sedt

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const tanmay_111915132_detail = new Schema({
+    eid: String,
+    firstName: String,
+    lastName: String,
+    dob: Date,
+    contactNumber: String,
+    password:String
+})
+
+const tanmay_111915132_salary = new Schema({
+    eid: String,
+    jobRole: String,
+    monthlySalary: String,
+    yearlyBonus:String
+})
+
+const Tanmay_111915132_detail = mongoose.model("Tanmay_111915132_detail", tanmay_111915132_detail);
+const Tanmay_111915132_salary = mongoose.model("Tanmay_111915132_salary",tanmay_111915132_salary);
+module.exports= {
+    tanmay_111915132_detail,
+    tanmay_111915132_salary
+}
